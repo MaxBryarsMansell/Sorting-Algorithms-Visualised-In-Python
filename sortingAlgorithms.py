c5c5637d5147380d33c600953b44f157bc8f671a
Max <maxbm46@gmail.com>
2017-10-15 11:36:11 +0100

    Display using dots property
    Implemented an option to display using dots rather than with bars. There is also now an option to display without colours so that the process is easier to see. The animation delay can now be changed through a property.

--- sortingAlgorithms.py
+++ sortingAlgorithms.py
@@ -7,68 +7,73 @@ from copy import copy
 # Properties
 w = 1280
 h = 720
-minElements = 100
-maxElements = 150
-animation_delay = 5 #in ms
+minElements = 150
+maxElements = 200
+animation_delay = 1 #in ms
+COLOUR_MODE = True
+DISPLAY_MODE_DOTS = True
 
 def bubbleSort(data):
-    swapped = None
-    while True:
-        swapped = False
-        for e in range(len(data)):
-            if (e) < len(data) - 1:
-                if data[e] > data[e + 1]:
-                    data[e], data[e + 1] = data[e + 1], data[e]
-                    swapped = True
+    for e in range(len(data) - 1, 0, -1):
+        for i in range(1, len(data)):
+            if data[i - 1] > data[i]:
+                data[i], data[i - 1] = data[i - 1], data[i]
                     
-                    queue.put(lambda: DrawList(data))
-                    queue.put(lambda: canvas.update())
-                    queue.put(lambda: canvas.delete("all"))
-                    time.sleep(animation_delay / 1000)
-        if not swapped:
-            print("Sorted with Bubble Sort")
-            break
+                queue.put(lambda: DrawList(data, i))
+                queue.put(lambda: canvas.update())
+                queue.put(lambda: canvas.delete("all"))
+                time.sleep(animation_delay / 1000)
+    print("Sorted with Bubble Sort")
+     
 
 def selectionSort(data):
     for e in range(len(data)):
         smallest = e
         for j in range(e + 1, len(data)):
             if data[j] < data[smallest]: smallest = j;
+
+            queue.put(lambda: DrawList(data, j))
+            queue.put(lambda: canvas.update())
+            queue.put(lambda: canvas.delete("all"))
+            time.sleep(animation_delay / 1000)
         temp = data[e]
         data[e] = data[smallest]
         data[smallest] = temp
         
-        queue.put(lambda: DrawList(data))
-        queue.put(lambda: canvas.update())
-        queue.put(lambda: canvas.delete("all"))
-        time.sleep(animation_delay / 1000)
+        
     print("Sorted with Selection Sort")
 
 def insertionSort(data):
-    for i in range(1, len(data)):
-        index = data[i]
-        j = i
+    for e in range(1, len(data)):
+        index = data[e]
+        j = e
         while j > 0 and data[j-1] > index:
             data[j] = data[j-1]
             j -= 1
-        data[j] = index
 
-        queue.put(lambda: DrawList(data))
-        queue.put(lambda: canvas.update())
-        queue.put(lambda: canvas.delete("all"))
-        time.sleep(animation_delay / 1000)
+            queue.put(lambda: DrawList(data, j))
+            queue.put(lambda: canvas.update())
+            queue.put(lambda: canvas.delete("all"))
+            time.sleep(animation_delay / 1000)
+        data[j] = index
     print("Sorted with Insertion Sort")
 
-def DrawList(data):
+def DrawList(data, current_selection):
     width = w / len(data)
     height = 0
     x, y = 0, h
-    for e in data:
-        depth = 256 - floor((256 / h) * e)
-        (r, g, b) = colorsys.hsv_to_rgb(float(depth) / 256, 1.0, 1.0)
-        R, G, B = int(255 * r), int(255 * g), int(255 * b)
-        colour = '#%02x%02x%02x' % (R, G, B)
-        canvas.create_rectangle(x, y , x + width, y - e, fill=colour)
+    for e in range(0, len(data)):
+        if not COLOUR_MODE: colour = "red";
+        else:
+            depth = 256 - floor((256 / h) * data[e])
+            (r, g, b) = colorsys.hsv_to_rgb(float(depth) / 256, 1.0, 1.0)
+            R, G, B = int(255 * r), int(255 * g), int(255 * b)
+            colour = '#%02x%02x%02x' % (R, G, B)
+        if e == current_selection: colour = "white";
+        if not DISPLAY_MODE_DOTS:
+            canvas.create_rectangle(x, y , x + width, y - data[e], fill=colour)
+        else:
+            canvas.create_oval(x, y - data[e], x + width, y - data[e] + width, fill=colour)
         x += width
 
 def Main():
