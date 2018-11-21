c54e65da10ffb0359ae5dd4f15cc8a74a574cbe4
Max <maxbm46@gmail.com>
2018-11-21 18:39:54 +0000

    new ui buttons and slider
    -added buttons for changing properties. -added slider for animation delay.

--- sortingAlgorithms.py
+++ sortingAlgorithms.py
@@ -1,6 +1,6 @@
 #Written By Max Bryars-Mansell
 
-import random, tkinter, os, colorsys, time, threading, queue
+import random, tkinter, os, colorsys, time, _thread, queue
 from math import floor
 from copy import copy
 from enum import Enum
@@ -9,22 +9,24 @@ from enum import Enum
 w = 1280
 h = 720
 minElements = 100
-maxElements = 500
-animation_delay = 1 #in ms
+maxElements = 1000
+ANIMATION_DELAY = 1 #in ms
 COLOUR_MODE = True
 DISPLAY_MODE_DOTS = False
 
 def bubbleSort(data):
+    global queue
     last = time.perf_counter()
     for e in range(len(data) - 1, 0, -1):
         for i in range(1, len(data)):
             if data[i - 1] > data[i]:
                 data[i], data[i - 1] = data[i - 1], data[i]
-
                 queue.put(lambda: DrawList(data, i))
-                time.sleep(animation_delay / 1000)
-    print("Sorted with Bubble Sort in", time.perf_counter() - last)
-    FinishedSorting()
+                time.sleep(ANIMATION_DELAY / 1000) 
+              
+    print("Sorted with Bubble Sort in", time.perf_counter() - last, "seconds.")
+    EndSort()
+    return
 
 def mergeSort(data):
     last = time.perf_counter()
@@ -53,12 +55,13 @@ def mergeSort(data):
                             data[j] = a[0]
                             a.remove(a[0])
 
-                queue.put(lambda: DrawList(data, k))
-                time.sleep(animation_delay / 1000)
+                queue.put(lambda: DrawList(data, i))
+                time.sleep(ANIMATION_DELAY / 1000)
             i = i + 2 * step
         step = step * 2
-    print("Sorted with Merge Sort in ",time.perf_counter() - last)
-    FinishedSorting()
+    print("Sorted with Merge Sort in ", time.perf_counter() - last, "seconds.")
+    EndSort()
+    return
 
 def selectionSort(data):
     last = time.perf_counter()
@@ -68,12 +71,13 @@ def selectionSort(data):
             if data[j] < data[smallest]: smallest = j;
 
             queue.put(lambda: DrawList(data, j))
-            time.sleep(animation_delay / 1000)
+            time.sleep(ANIMATION_DELAY / 1000)
         temp = data[e]
         data[e] = data[smallest]
         data[smallest] = temp
-    print("Sorted with Selection Sort in ", time.perf_counter() - last)
-    FinishedSorting()
+    print("Sorted with Selection Sort in ", time.perf_counter() - last, "seconds.")
+    EndSort()
+    return
 
 def insertionSort(data):
     last = time.perf_counter()
@@ -85,23 +89,24 @@ def insertionSort(data):
             j -= 1
 
             queue.put(lambda: DrawList(data, j))
-            time.sleep(animation_delay / 1000)
+            time.sleep(ANIMATION_DELAY / 1000)
         data[j] = index
-    print("Sorted with Insertion Sort in ", time.perf_counter() - last)
-    FinishedSorting()
+    print("Sorted with Insertion Sort in ", time.perf_counter() - last, "seconds.")
+    EndSort()
+    return
 
 def DrawList(data, current_selection):
     width = w / len(data)
     height = 0
     x, y = 0, h
     for e in range(0, len(data)):
-        if not COLOUR_MODE: colour = "red";
+        if not COLOUR_MODE: colour = "red"
         else:
             depth = 256 - floor((256 / h) * data[e])
             (r, g, b) = colorsys.hsv_to_rgb(float(depth) / 256, 1.0, 1.0)
             R, G, B = int(255 * r), int(255 * g), int(255 * b)
             colour = '#%02x%02x%02x' % (R, G, B)
-        if e == current_selection: colour = "white";
+        if e == current_selection: colour = "white"
         if not DISPLAY_MODE_DOTS:
             canvas.create_rectangle(x, y , x + width, y - data[e], fill=colour)
         else:
@@ -112,70 +117,104 @@ def Cleanup():
     window.quit()
     os._exit(0)
 
-def FinishedSorting():
-    global sorting, finished
-    time.sleep(2)
-    sorting = False
+def BeginSort(sort):
+    global sorting, lst, queue
+    with queue.mutex: queue.queue.clear()
+    if (not sorting):
+        if (sort == "BUBBLE"):
+            _thread.start_new_thread(bubbleSort, (lst, ))
+        if (sort == "MERGE"):
+            _thread.start_new_thread(mergeSort, (lst, ))
+        if (sort == "SELECTION"):
+            _thread.start_new_thread(selectionSort, (lst, ))
+        if (sort == "INSERTION"):
+            _thread.start_new_thread(insertionSort, (lst, ))
+        sorting = True
+
+def EndSort():
+    global finished
     finished = True
+
+def ToggleColourMode():
+    global COLOUR_MODE
+    if (COLOUR_MODE == True):
+        COLOUR_MODE = False
+    else:
+        COLOUR_MODE = True
+
+def ToggleDotMode():
+    global DISPLAY_MODE_DOTS
+    if (DISPLAY_MODE_DOTS == True):
+        DISPLAY_MODE_DOTS = False
+    else:
+        DISPLAY_MODE_DOTS = True
+
+def ResetList(value):
+    global lst, sorting
+    if (not sorting):
+        lst = random.sample(range(0, h - 10), int(value))
+
+def UpdateDelay(value):
+    global ANIMATION_DELAY
+    ANIMATION_DELAY = int(value)
 
 window = tkinter.Tk()
 window.protocol("WM_DELETE_WINDOW", Cleanup)
 window.title("Sorting Algorithms Visualised")
-canvas = tkinter.Canvas(window, bg="black", height=h, width=w)
-canvas.pack()
 queue = queue.Queue()
 lst = random.sample(range(0, h - 10), minElements)
 sorting = False
 finished = False
 
-def beginSort(sort):
-    global sorting, lst
-    thread = threading.Thread()
-    thread.daemon = True
-    if (not thread.isAlive() and not sorting):
-        with queue.mutex:
-            queue.queue.clear()
-        if (sort == "BUBBLE"):
-            thread._target=lambda: bubbleSort(lst)
-        if (sort == "MERGE"):
-            thread._target=lambda: mergeSort(lst)
-        if (sort == "SELECTION"):
-            thread._target=lambda: selectionSort(lst)
-        if (sort == "INSERTION"):
-            thread._target=lambda: insertionSort(lst)
-    if (not sorting):
-        sorting = True
-        thread.start()
+toolbar = tkinter.Frame(window)
 
-BubbleSort = tkinter.Button(window, text="Bubble Sort", command =  lambda: beginSort("BUBBLE"))
+ColourMode = tkinter.Button(toolbar, text="Toggle Colour Mode", command = ToggleColourMode)
+ColourMode.pack(side="right")
+
+DotMode = tkinter.Button(toolbar, text="Toggle Dot Mode", command = ToggleDotMode)
+DotMode.pack(side="right")
+
+toolbar.pack(side="top", fill="x")
+
+canvas = tkinter.Canvas(window, bg="black", height=h, width=w)
+canvas.pack()
+
+BubbleSort = tkinter.Button(window, text="Bubble Sort", command =  lambda: BeginSort("BUBBLE"))
 BubbleSort.pack(side="left")
 
-MergeSort = tkinter.Button(window, text="Merge Sort", command = lambda: beginSort("MERGE"))
+MergeSort = tkinter.Button(window, text="Merge Sort", command = lambda: BeginSort("MERGE"))
 MergeSort.pack(side="left")
 
-SelectionSort = tkinter.Button(window, text="Selection Sort", command = lambda: beginSort("SELECTION"))
+SelectionSort = tkinter.Button(window, text="Selection Sort", command = lambda: BeginSort("SELECTION"))
 SelectionSort.pack(side="left")
 
-InsertionSort = tkinter.Button(window, text="Insertion Sort", command = lambda: beginSort("INSERTION"))
+InsertionSort = tkinter.Button(window, text="Insertion Sort", command = lambda: BeginSort("INSERTION"))
 InsertionSort.pack(side="left")
 
-slider = tkinter.Scale(window, from_=minElements, to=maxElements, orient="horizontal", length=250)
-slider.value=maxElements
-slider.pack(side="right")
+toolbar2 = tkinter.Frame(window)
 
-sliderValue = slider.get()
-def setLst():
-    global sliderValue, lst
-    lst = random.sample(range(0, h - 10), slider.get())
-    sliderValue = slider.get()
+ElementSlider = tkinter.Scale(toolbar2, from_=minElements, to=maxElements, orient="horizontal", length=250, command = ResetList)
+ElementSlider.pack(side="right")
+
+Label1 = tkinter.Label(toolbar2, text="Element Count:")
+Label1.pack(side="right")
+
+DelaySlider = tkinter.Scale(toolbar2, from_=1, to=1000, orient="horizontal", length=250, command = UpdateDelay)
+DelaySlider.pack(side="right")
+
+Label2 = tkinter.Label(toolbar2, text="Animation Delay (ms):")
+Label2.pack(side="right")
+
+toolbar2.pack(side="bottom", fill="x")
 
 while True:
     window.update()
     window.update_idletasks()
     canvas.delete("all")
-    if (sliderValue != slider.get() and not sorting or finished):
-         setLst()
-         finished = False
+    if (finished):
+        finished = False
+        sorting = False
+        ResetList(ElementSlider.get())
     if (sorting):
         function = queue.get()
         function()
